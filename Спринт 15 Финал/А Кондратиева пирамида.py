740b2ff4d90d1738becc4e68ab2cd642ce2de599
afrlv1 <afrlv@yandex.ru>
2020-09-04 12:42:55 +0500

    After CR Sprint 15

--- Спринт 15 Финал/А Кондратиева пирамида.py
+++ Спринт 15 Финал/А Кондратиева пирамида.py
@@ -33,72 +33,66 @@
 # sam 1 9 5 0 1
 # anna 1 -4 5 0 1
 # номер посылки 33998378
-class InputData:
-    def __init__(self, data, pos):
-        self.data = data
-        data = data.split(' ')
-        self.score = sum([int(i) for i in data[1:] if int(i) > 0])
-        self.name = data[0]
-        self.rule = set('kondratiy').issubset(self.name)
-        self.pos = pos
-
-    def __gt__(self, other):
-        if self.rule and other.rule:
-            return self.pos > other.pos
-        elif not self.rule and not other.rule:
-            if self.score == other.score:
-                if self.name == other.name:
-                    return self.pos > other.pos
-                return self.name < other.name
-            return self.score > other.score
-        elif self.rule and not other.rule:
-            return True
-        elif not self.rule and other.rule:
-            return False
-
-    def __repr__(self):
-        return self.data
+from collections import namedtuple
+Record = namedtuple('Record', 'place name rate sequence')
 
 
-def heapify(nums, heap_size, root_index):
-    # индекс наибольшего элемента считается корневым индексом
-    largest = root_index
-    left_child = (2 * root_index) + 1
-    right_child = (2 * root_index) + 2
-    # Если левый потомок корня - это допустимый индекс, а элемент больше
-    # чем текущий наибольший, то мы обновляем наибольший элемент
-    if left_child < heap_size and nums[largest] > nums[left_child]:
-        largest = left_child
-    # тоже самое для правого потомка корня
-    if right_child < heap_size and nums[largest] > nums[right_child]:
-        largest = right_child
-
-    # если наибольший элемент уже не корневой, они меняются местами
-    if largest != root_index:
-        nums[root_index], nums[largest] = nums[largest], nums[root_index]
-        heapify(nums, heap_size, largest)
+def _sort_lt_(self, other):
+    if self.rate != other.rate:
+        return self.rate < other.rate
+    elif self.name != other.name:
+        return self.name > other.name
+    else:
+        return self.place < other.place
 
 
-def heapsort(nums, n):
-    """создаем Max heap из списка
-    второй аргумент означает остановку алгоритма перед элементом -1, то есть
-    перед первым элементом списка
-    3-ий аргумент означает повторный проход по списку в обратном направлении,
-    уменьшая счетчик i на 1
-    """
-    for i in range(n, -1, -1):
-        heapify(nums, n, i)
-    # перемещаем корень Max Heap в самый конец списка
-    for i in range(n - 1, 0, -1):
-        nums[i], nums[0] = nums[0], nums[i]
-        heapify(nums, i, 0)
+setattr(Record, '__lt__', _sort_lt_)
 
-if __name__ == "__main__":
-    heap = []
-    with open('input.txt') as f:
-        n = int(f.readline())
-        for pos in range(n):
-            heap.append(InputData(f.readline().strip(), pos))
-    heapsort(heap, n)
-    for elem in heap:
-        print(elem)
+
+def is_name(name):
+    return set('kondratiy').issubset(name)
+
+
+def heapify(arr, arr_length, index):
+    largest = index
+    left = 2 * index + 1
+    right = 2 * index + 2
+    if left < arr_length and arr[index] < arr[left]:
+        largest = left
+    if right < arr_length and arr[largest] < arr[right]:
+        largest = right
+    if largest != index:
+        arr[index], arr[largest] = arr[largest], arr[index]
+        heapify(arr, arr_length, largest)
+
+
+def pyramid_sort(arr):
+    length = len(arr)
+    for index in range(length // 2 - 1, -1, -1):
+        heapify(arr, length, index)
+    for index in range(length - 1, 0, -1):
+        arr[index], arr[0] = arr[0], arr[index]
+        heapify(arr, index, 0)
+
+
+if __name__ == '__main__':
+    with open('input.txt') as fd:
+        buff = fd.read().splitlines()
+        n = int(buff[0])
+        top = []
+        table = []
+        for place, number in enumerate(range(1, n + 1)):
+            record = buff[number].split()
+            name = record[0]
+            sequence = record[1:]
+            rate = sum(int(x) for x in sequence if int(x) >= 0)
+            if is_name(name):
+                top.append(Record(place, name, rate, sequence))
+            else:
+                table.append((Record(place, name, rate, sequence)))
+    if top:
+        for item in top[::-1]:
+            print(item.name, *item.sequence)
+    pyramid_sort(table)
+    for item in table[::-1]:
+        print(item.name, *item.sequence)
